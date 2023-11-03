ee97fb14e4a77ca1abd447a7eafadf86299a2087
Daniel Scerpa <daniel.scerpa@gmail.com>
2023-11-03 20:26:44 +0100

    Added model ChartData.cs

---
+++ Razor_WebAPI_jQuery_AJAX/Models/ChartData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_WebAPI_Application.Models
+{
+        public class ChartData
+        {
+            public List<double> X { get; set; }
+            public List<double> Y { get; set; }
+    }
+}
