5adc7bd5416c95a62e278a80fee6b5ec971b3b29
Daniel Scerpa <daniel.scerpa@gmail.com>
2023-11-03 20:27:01 +0100

    Added page with button and chart Index.cshtml

---
+++ Razor_WebAPI_jQuery_AJAX/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor_WebAPI_Application.Models;
+
+namespace Razor_WebAPI_Application.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
